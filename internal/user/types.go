cedaed4e715a71accf635dca774fcaea648b420f
amirjbr <amirreza.jabbari2023@gmail.com>
2024-11-30 20:01:04 +0330

    feat: adding national code Validation and email Validation

--- internal/user/types.go
+++ internal/user/types.go
@@ -2,12 +2,18 @@ package user
 
 import (
 	"errors"
+	"regexp"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 var (
 	ErrorWhileGeneratingHashPassword = "faield to hash password"
+	ErrorInvalidEmail                = "invalid email format"
+	ErrorInvalidNationalCode         = "invalid national code . must be 10 digits"
+	emailRegex                       = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	nationalCodeRegex                = regexp.MustCompile(`^\d{10}$`)
 )
 
 func HashPassword(password string) (string, error) {
@@ -21,4 +27,20 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hashedPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+func ValidateEmail(email string) (string, error) {
+	email = strings.TrimSpace(email)
+	if !emailRegex.MatchString(email) {
+		return "", errors.New(ErrorInvalidEmail)
+	}
+	return strings.ToLower(email), nil
+}
+
+func ValidateNationalCode(nationalCode string) (string, error) {
+	nationalCode = strings.TrimSpace(nationalCode)
+	if !nationalCodeRegex.MatchString(nationalCode) {
+		return "", errors.New(ErrorInvalidNationalCode)
+	}
+	return nationalCode, nil
 }
