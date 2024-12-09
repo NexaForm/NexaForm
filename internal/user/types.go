0266f1dc5b77c4870d5cc03ec7be38b1df8e9b22
amirjbr <amirreza.jabbari2023@gmail.com>
2024-12-09 14:28:08 +0330

    feat: validation for national code and email and hashPass functionality

--- internal/user/types.go
+++ internal/user/types.go
@@ -7,9 +7,11 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // GenderType defines valid gender values
@@ -21,11 +23,17 @@ const (
 )
 
 var (
-	ErrUserNotFound          = errors.New("user not found")
-	ErrInvalidEmail          = errors.New("invalid email format")
-	ErrInvalidPassword       = errors.New("invalid password format")
-	ErrEmailAlreadyExists    = errors.New("email already exists")
-	ErrInvalidAuthentication = errors.New("email and password doesn't match")
+	ErrUserNotFound                    = errors.New("user not found")
+	ErrInvalidEmail                    = errors.New("invalid email format")
+	ErrInvalidPassword                 = errors.New("invalid password format")
+	ErrEmailAlreadyExists              = errors.New("email already exists")
+	ErrInvalidAuthentication           = errors.New("email and password doesn't match")
+	ErrorWhileGeneratingHashPassword   = "faield to hash password"
+	ErrorInvalidEmail                  = "invalid email format"
+	ErrorInvalidNationalCodeDigits     = "national code must be exactly 10 digits"
+	ErrorInvalidNationalCodeOnlyDigits = "national code must contain only digits"
+	ErrorInvalidNationalCode           = "invalid national code"
+	emailRegex                         = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 )
 
 type Repo interface {
@@ -96,4 +104,65 @@ func ValidatePasswordWithFeedback(password string) error {
 
 func LowerCaseEmail(email string) string {
 	return strings.ToLower(email)
+}
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", errors.New(ErrorWhileGeneratingHashPassword)
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPasswordHash(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+// func ValidateEmail(email string) (string, error) {
+// 	email = strings.TrimSpace(email)
+// 	if !emailRegex.MatchString(email) {
+// 		return "", errors.New(ErrorInvalidEmail)
+// 	}
+// 	return strings.ToLower(email), nil
+// }
+
+func ValidateNationalCode(code string) (string, error) {
+	if len(code) != 10 {
+		return "", errors.New(ErrorInvalidNationalCodeDigits)
+	}
+
+	if _, err := strconv.Atoi(code); err != nil {
+		return "", errors.New(ErrorInvalidNationalCodeOnlyDigits)
+	}
+
+	if code == "0000000000" {
+		return "", errors.New(ErrorInvalidNationalCode)
+	}
+	if code == "9876543210" {
+		return "", errors.New(ErrorInvalidNationalCode)
+	}
+	digits := make([]int, 10)
+	for i, c := range code {
+		digits[i], _ = strconv.Atoi(string(c))
+	}
+
+	var B int
+	for i := 0; i < 9; i++ {
+		B += digits[i] * (10 - i)
+	}
+
+	C := B - (B/11)*11
+	A := digits[9]
+
+	if C == 0 && A == C {
+		return code, nil
+	}
+	if C == 1 && A == 1 {
+		return code, nil
+	}
+	if C > 1 && A == (11-C) {
+		return code, nil
+	}
+
+	return "", errors.New(ErrorInvalidNationalCode)
 }
